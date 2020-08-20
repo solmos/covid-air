0d7173c4d32419d85a07e5377a7517656225ed2b
solmos <sergio.olmos@isglobal.org>
2020-08-20 11:06:42 +0200

    Modified exposure variances and changed range of HR

--- R/plan.R
+++ R/plan.R
@@ -1,36 +1,37 @@
 plan <- drake_plan(
   ## Exposure data
-  exposures = cleanExposureData(),
-  pm25_var_aim1 = var(exposures$pm25),
-  pm25_var_aim2 = getAnnualVariance(exposures),
+  exposure_annual = readAnnualExposure(),
+  exposure_daily = readDailyExposure(),
+  pm25_var_aim1 = getAnnualVariance(exposure_annual, model = "apache"),
+  pm25_var_aim2 = getDailyVariance(exposure_daily),
 
   ## Incidence
   n_population = 5000000,
   n_cases = 115000,
-  hospitalizations = 37800,
+  hospitalizations = 32000,
   deaths = 13000,
 
   ## Aim 1: Long-term exposure
   hospi_power_aim1 = calcPowerDefault(
     n = n_population, events = hospitalizations, sigma2 = pm25_var_aim1,
-    hr.min = 1, hr.max = 1.1
+    hr.min = 1, hr.max = 1.06
   ),
   hospi_plot_aim1 = plotPowerDefault(hospi_power_aim1),
   dead_power_aim1 = calcPowerDefault(
     n = n_population, events = deaths, sigma2 = pm25_var_aim1,
-    hr.min = 1, hr.max = 1.1
+    hr.min = 1, hr.max = 1.06
   ),
   dead_plot_aim1 = plotPowerDefault(dead_power_aim1),
 
   ## Aim 2: Short-term exposure
   hospi_power_aim2 = calcPowerDefault(
     n = n_cases, events = hospitalizations, sigma2 = pm25_var_aim2,
-    hr.min = 1, hr.max = 1.1
+    hr.min = 1, hr.max = 1.06
   ),
   hospi_plot_aim2 = plotPowerDefault(hospi_power_aim2),
   dead_power_aim2 = calcPowerDefault(
     n = n_cases, events = deaths, sigma2 = pm25_var_aim2,
-    hr.min = 1, hr.max = 1.1
+    hr.min = 1, hr.max = 1.06
   ),
   dead_plot_aim2 = plotPowerDefault(dead_power_aim2),
   power_report = rmarkdown::render(
